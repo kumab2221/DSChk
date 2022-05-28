c82a44f8d4d97943918d19089125e344c3e34344
kumab2221 <kumab2221@gmail.com>
2022-05-28 15:33:27 +0900

    update 連続アウトプット表示機能追加
    DatagridViewに記載のフォルダパスのサイズをCSV形式で一括出力する機能を追加

--- DSChk/Form1.Designer.cs
+++ DSChk/Form1.Designer.cs
@@ -36,14 +36,20 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +61,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(634, 336);
+            this.tabControl1.Size = new System.Drawing.Size(640, 291);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -68,7 +74,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(626, 308);
+            this.tabPage1.Size = new System.Drawing.Size(632, 263);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "個別参照";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -76,7 +82,7 @@
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(434, 277);
+            this.button3.Location = new System.Drawing.Point(440, 231);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
@@ -87,7 +93,7 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(532, 277);
+            this.button2.Location = new System.Drawing.Point(538, 230);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -100,9 +106,9 @@
             this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.treeView1.Location = new System.Drawing.Point(19, 59);
+            this.treeView1.Location = new System.Drawing.Point(19, 58);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(588, 204);
+            this.treeView1.Size = new System.Drawing.Size(594, 162);
             this.treeView1.TabIndex = 2;
             this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
             // 
@@ -113,7 +119,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Location = new System.Drawing.Point(19, 16);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(490, 23);
+            this.textBox1.Size = new System.Drawing.Size(496, 23);
             this.textBox1.TabIndex = 1;
             this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
             this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
@@ -121,7 +127,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(532, 16);
+            this.button1.Location = new System.Drawing.Point(538, 16);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -131,23 +137,88 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Controls.Add(this.button4);
             this.tabPage2.Controls.Add(this.statusStrip1);
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(626, 308);
+            this.tabPage2.Size = new System.Drawing.Size(632, 263);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "全体出力";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(8, 205);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "クリア";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(636, 193);
+            this.panel1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(636, 193);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column1.HeaderText = "選択";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 37;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "パス";
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(544, 205);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "出力";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripProgressBar1,
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(3, 283);
+            this.statusStrip1.Location = new System.Drawing.Point(3, 238);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(620, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(626, 22);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -166,15 +237,19 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 336);
+            this.ClientSize = new System.Drawing.Size(640, 291);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "DSChk";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -188,13 +263,17 @@
         private TabPage tabPage2;
         private TextBox textBox1;
         private Button button1;
-        private StatusStrip statusStrip1;
-        private ToolStripProgressBar toolStripProgressBar1;
-        private ToolStripStatusLabel toolStripStatusLabel1;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.ComponentModel.BackgroundWorker backgroundWorker2;
         private TreeView treeView1;
         private Button button3;
         private Button button2;
+        private Panel panel1;
+        private DataGridView dataGridView1;
+        private DataGridViewCheckBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private Button button4;
+        private StatusStrip statusStrip1;
+        private ToolStripProgressBar toolStripProgressBar1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private Button button5;
     }
 }
--- DSChk/Form1.cs
+++ DSChk/Form1.cs
@@ -1,3 +1,5 @@
+using System.Collections.Specialized;
+
 namespace DSChk
 {
     public partial class Form1 : Form
@@ -52,6 +54,59 @@ namespace DSChk
 
         private void button3_Click(object sender, EventArgs e)
         {
+            MakeTreeView();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            string fileName = "Output" + dt.ToString("yyyyMMdd") + ".csv";
+            using (StreamWriter sw = new StreamWriter(fileName, false, System.Text.Encoding.UTF8))
+            {
+                DirectoryInfo dirList = new DirectoryInfo(textBox1.Text);
+                OutputDirectorySize(sw, dirList, 0);
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                TreeNode? node = e.Node;
+                String? path = node?.FullPath;
+
+                if (path == null) return;
+                path = System.Text.RegularExpressions.Regex.Replace(path, " \\([0-9,]*KB\\)", "");
+                if (hashlist.Contains(path))
+                    return;
+
+                node?.Nodes.Clear();
+
+                DirectoryInfo dirList = new DirectoryInfo(path);
+
+                //サブフォルダのサイズを合計していく
+                foreach (DirectoryInfo di in dirList.GetDirectories())
+                {
+                    var subNode = node?.Nodes.Add(di.Name);
+                    long ssize = 0;
+                    ssize = SetNode(subNode, di, false);
+                    if (subNode is not null)
+                        subNode.Text += " (" + (ssize / 1024).ToString("#,0") + "KB)";
+                }
+
+                hashlist.Add(path);
+
+            }
+            catch (IOException ie)
+            {
+
+            }
+
+        }
+
+        private void MakeTreeView()
+        {
+
             treeView1.Nodes.Clear();
             var node = treeView1.Nodes.Add(textBox1.Text);
             var dirList = new DirectoryInfo(textBox1.Text);
@@ -69,8 +124,8 @@ namespace DSChk
             }
 
             node.Text += " (" + (size / 1024).ToString("#,0") + "KB)";
-
         }
+
 
         private long SetNode(TreeNode? node, DirectoryInfo dirInfo, bool recursion)
         {
@@ -100,16 +155,7 @@ namespace DSChk
             return csize;
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            DateTime dt = DateTime.Now;
-            string fileName = "Output" + dt.ToString("yyyyMMdd") + ".csv";
-            using (StreamWriter sw = new StreamWriter(fileName, false, System.Text.Encoding.UTF8))
-            {
-                DirectoryInfo dirList = new DirectoryInfo(textBox1.Text);
-                OutputDirectorySize(sw, dirList, 0);
-            }
-        }
+        
 
         public static long OutputDirectorySize( StreamWriter sw, DirectoryInfo dirInfo, int count)
         {
@@ -129,40 +175,94 @@ namespace DSChk
             return size;
         }
 
-        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            try
+            textBox1.Text = Properties.Settings.Default.path;
+
+            dataGridView1.RowCount = 100;
+            var pathLit = Properties.Settings.Default.pathList;
+            var outputList = Properties.Settings.Default.outputFlg;
+            if (pathLit == null || outputList == null)
             {
-                TreeNode? node = e.Node;
-                String? path = node?.FullPath;
-
-                if (path == null) return;
-                path = System.Text.RegularExpressions.Regex.Replace(path, " \\([0-9,]*KB\\)", "");
-                if(hashlist.Contains(path))
-                    return;
-                
-                node?.Nodes.Clear();
-
-                DirectoryInfo dirList = new DirectoryInfo(path);
-
-                //サブフォルダのサイズを合計していく
-                foreach (DirectoryInfo di in dirList.GetDirectories())
+                for (int i = 0; i < 100; i++)
                 {
-                    var subNode = node?.Nodes.Add(di.Name);
-                    long ssize = 0;
-                    ssize = SetNode(subNode, di, false);
-                    if (subNode is not null)
-                        subNode.Text += " (" + (ssize / 1024).ToString("#,0") + "KB)";
+                    dataGridView1[1, i].Value = "";
+                    dataGridView1[0, i].Value = false;
                 }
-
-                hashlist.Add(path);
-
+                return;
             }
-            catch (IOException ie)
+            for (int i=0; i<100; i++)
             {
+                dataGridView1[1, i].Value = pathLit[i];
+                if (outputList[i] == "0")
+                    dataGridView1[0, i].Value = false;
+                else
+                    dataGridView1[0, i].Value = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.path = textBox1.Text;
+
+            string[] pathArray = new string[100];
+            string[] flgArray = new string[100];
+            
+            var pathCol = new StringCollection();
+            var flgCol = new StringCollection();
+
+            for (int i = 0; i < 100; i++)
+            {
+                pathArray[i] = dataGridView1[1, i].Value?.ToString() ?? "";
+
+                bool flg = (bool)(dataGridView1[0, i].Value);
+                if (flg)
+                    flgArray[i] = "1";
+                else
+                    flgArray[i] = "0";
 
             }
 
+            pathCol.AddRange(pathArray);
+            flgCol.AddRange(flgArray);
+
+            Properties.Settings.Default.pathList = pathCol;
+            Properties.Settings.Default.outputFlg = flgCol;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                dataGridView1[0, i].Value = false;
+                dataGridView1[1, i].Value = "";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            var dirName = "output" + dt.ToString("yyyyMMdd");
+            if (!System.IO.Directory.Exists(dirName))
+                System.IO.Directory.CreateDirectory(dirName);
+
+            for (int i = 0; i < 100; ++i)
+            {
+                if ((bool)dataGridView1[0, i].Value == false) continue;
+
+                var path = dataGridView1[1, i].Value.ToString();
+                if (System.IO.Directory.Exists(path) == false) continue;
+
+                var fileName = dirName + "\\" + dt.ToString("yyyyMMdd") + "_" + i.ToString() + ".csv";
+                using (StreamWriter sw = new StreamWriter(fileName, false, System.Text.Encoding.UTF8))
+                {
+                    DirectoryInfo dirList = new DirectoryInfo(path);
+                    OutputDirectorySize(sw, dirList, 0);
+                }
+            }
+            MessageBox.Show("完了しました");
         }
     }
 }
